fbba9d4e6c578dded4c1a236aa539591ce06e33e
Aaron Kaswen-Wilk <aaron.kaswen-wilk@qonto.com>
2025-01-23 15:12:43 +0100

    initial new and purge commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::{Parser, Subcommand};
+pub mod new;
+pub mod purge;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    cmd: Commands,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+enum Commands {
+    New,
+    Purge,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.cmd {
+        Commands::New => new::run(),
+        Commands::Purge => purge::run(),
+    }
 }
