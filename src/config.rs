d2900fd18f6017c7b7b07355d544a3f303158125
Aaron Kaswen-Wilk <aaron.kaswen-wilk@qonto.com>
2025-01-26 10:17:51 +0100

    implement purge

--- src/config.rs
+++ src/config.rs
@@ -1,26 +1,56 @@
-use std::collections::HashMap;
+use serde::Deserialize;
 use std::{env, fs};
 
-pub type Config = HashMap<Username, ReposConfig>;
-pub type Username = String;
+#[derive(Debug, Deserialize, Clone)]
+pub struct Config {
+    #[serde(default)]
+    pub config: SpaceConfig,
+    pub repos: ReposList,
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct SpaceConfig {
+    #[serde(default = "default_spaces_dir")]
+    pub spaces_dir: String,
+}
+
+impl Default for SpaceConfig {
+    fn default() -> Self {
+        SpaceConfig {
+            spaces_dir: default_spaces_dir(),
+        }
+    }
+}
+
+pub type ReposList = Vec<RepoList>;
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct RepoList {
+    pub username: String,
+    pub token: String,
+    pub repos: Vec<Repo>,
+}
+
 pub type Repo = String;
-pub type ReposConfig = HashMap<Token, Repos>;
-pub type Repos = Vec<Repo>;
-pub type Token = String;
 
 pub fn load() -> Config {
     let spaces_file = env::var("SPACES_CONFIG").unwrap_or(default_spaces_file());
-
-    println!("Loading config from {:?}", spaces_file);
 
     let data = fs::read_to_string(spaces_file).expect("Unable to read file");
     serde_yaml::from_str(&data).unwrap()
 }
 
 const DEFAULT_SPACES_FILE_NAME: &str = ".spaces.yml";
+const DEFAULT_SPACES_DIR: &str = "spaces";
 
 fn default_spaces_file() -> String {
     let home = dirs::home_dir().unwrap();
     let spaces_file = home.join(DEFAULT_SPACES_FILE_NAME);
     spaces_file.to_str().unwrap().to_string()
+}
+
+fn default_spaces_dir() -> String {
+    let home = dirs::home_dir().unwrap();
+    let spaces_dir = home.join(DEFAULT_SPACES_DIR);
+    spaces_dir.to_str().unwrap().to_string()
 }
--- src/purge.rs
+++ src/purge.rs
@@ -1,5 +1,11 @@
 use crate::config;
+use std::fs;
 
 pub fn run(conf: config::Config) {
-    println!("Running purge command with config! {:?}", conf);
+    if fs::read_dir(conf.config.spaces_dir.clone()).is_err() {
+        println!("no spaces found in {:?}", conf.config.spaces_dir);
+        return;
+    }
+    fs::remove_dir_all(conf.config.spaces_dir.clone()).unwrap();
+    println!("all spaces in {:?} purged successfully", conf.config.spaces_dir);
 }
